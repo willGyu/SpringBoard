7fb40a4d123008e9b7896edb63bee1f5d0fbfb53
willGyu <jgt8731@itwillbs.co.kr>
2024-02-20 17:50:14 +0900

    도메인객체(BoardVO) 생성

---
+++ SpringBoard/src/main/java/com/itwillbs/domain/BoardVO.java
@@ -0,0 +1,17 @@
+package com.itwillbs.domain;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+
+	private int bno;
+	private String title;
+	private String content;
+	private String writer;
+	private int viewcnt;
+	private Timestamp regdate;
+	
+}
