28b3fe3f7635d8e1a7462950ce30dae24ab76c49
willGyu <jgt8731@itwillbs.co.kr>
2024-02-29 11:16:58 +0900

    AOP 테스트 완료

---
+++ SpringBoard/src/main/java/com/itwillbs/aop/CalcTest.java
@@ -0,0 +1,19 @@
+package com.itwillbs.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CalcTest {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext CTX 
+		      = new ClassPathXmlApplicationContext("aop.xml");
+		
+		Calculator c = (Calculator) CTX.getBean("proxyCal");
+		
+		c.add(100, 200);
+
+	}
+
+}
